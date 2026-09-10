f8ef0e6d5a7479d3408466ec3741bb038f2a11c7
Inbi-Nav <inbi.nav.02@gmail.com>
2026-09-10 13:42:46 +0200

    feat: project restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import "./App.css";
-import { FloatingNav } from "./components/layout/FloatingNav";
-import { Hero } from "./components/Hero";
-import { CustomCursor } from "./components/ui/CustomCursor";
 
 function App() {
   return (
     <>
-      <CustomCursor />
-      <FloatingNav />
-
-      <section id="home" style={{ height: "100vh" }}>
-        <Hero />
-      </section>
-
-      <section id="about" style={{ height: "100vh" }}>
-        <h1>About</h1>
-      </section>
-
-      <section id="projects" style={{ height: "100vh" }}>
-        <h1>Projects</h1>
-      </section>
-
-      <section id="stack" style={{ height: "100vh" }}>
-        <h1>Stack</h1>
-      </section>
-
-      <section id="contact" style={{ height: "100vh" }}>
-        <h1>Contact</h1>
-      </section>
+      
     </>
   );  
 }
