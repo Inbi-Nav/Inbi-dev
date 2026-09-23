0836f1e22792a19f1c2c51420a26af35cc84d9c8
Inbi-Nav <inbi.nav.02@gmail.com>
2026-09-23 13:41:33 +0200

    feat: add project theme and implement navbar with navigation, dark mode and language toggles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,70 +1,95 @@
-import { useState } from "react";
-import "./Navbar.css";
-type Language = 'en' | 'es';
+    import { useState, useEffect } from "react";
+    import "./Navbar.css";
+    import darkModeLogo from "../../assets/logo_light_mode.png";
+    import lightModeLogo from "../../assets/logo_dark_mode.png";
+    import darkModeSign from "../../assets/light_mode_sign.png";
+    import lightModeSign from "../../assets/dark_mode_sign.png";
 
-    const translations = {
-        es: {
-            home: "Inicio",
-            about:"Sobre Mi",
-            skills:"Skills",
-            projects:"Proyectos",
-            contact:"Contacto"
-        },
-        en: {
-            home: "Home",
-            about: "About Me",
-            skills: "Skills",
-            projects: "Projects",
-            contact: "Contact"
-        },
+    type Language = 'en' | 'es';
+
+        const translations = {
+            es: {
+                home: "INICIO",
+                about:"SOBRE MI",
+                skills:"SKILLS",
+                projects:"PROYECTOS",
+                contact:"CONTACTO"
+            },
+            en: {
+                home: "HOME",
+                about: "ABOUT ME",
+                skills: "SKILLS",
+                projects: "PROJECTS",
+                contact: "CONTACT"
+            },
+        }
+
+    function Navbar() {
+
+        const [isDarkMode, setIsDarkMode] = useState(false);
+        const toggleDarkMode = () => {
+            setIsDarkMode((previousMode) => !previousMode);
+        };
+
+        useEffect(() => {
+            if (isDarkMode) {
+                document.documentElement.setAttribute("data-theme", "dark");
+            } else {
+                document.documentElement.setAttribute("data-theme", "light");
+            }
+        }, [isDarkMode]);
+        
+        const [language, setLanguage] = useState<Language>("es");
+
+        const toggleLanguage = () => {
+            setLanguage((prevLanguage) => (prevLanguage === "es" ? "en" : "es"));
+        };
+
+        const text = translations[language];
+        return (
+            <nav className="navbar">
+
+            <div className="navbar-logo">
+                    <img src={isDarkMode ? darkModeLogo : lightModeLogo} alt="Logo" />
+                </div>
+
+                <div className="navbar-links">
+                    <a href="#home">{text.home}</a>
+                    <a href="#about">{text.about}</a>
+                    <a href="#skills">{text.skills}</a>
+                    <a href="#projects">{text.projects}</a>
+                    <a href="#contact">{text.contact}</a>
+                </div>
+
+                <div className="navbar-actions">
+                    <div className= "darkMode-toggle">
+                        <button onClick={toggleDarkMode}>
+                            <img src={isDarkMode ? darkModeSign : lightModeSign} alt={isDarkMode ? "Light Mode" : "Dark Mode"}/>
+                        </button>
+                        </div> 
+
+                    <div className="language-toggle">
+                        <button
+                            className={`language-switch ${language}`}
+                            onClick={toggleLanguage}
+                            aria-label={language === "es"? "Change language to English" : "Cambiar idioma a español"
+                            }>
+                            <span className="language-option language-en">
+                                EN
+                            </span>
+                        
+                            <span className="language-option language-es">
+                                ES
+                            </span>
+                        
+                            <span className="language-circle">
+                                {language === "es" ? "ES" : "EN"}
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </nav>
+        );
+
     }
-
-function Navbar() {
-
-    const [isDarkMode, setIsDarkMode] = useState(false);
-    const toggleDarkMode = () => {
-        setIsDarkMode(!isDarkMode);
-    }
-    // language change function
-    const [language, setLanguage] = useState<Language>("es");
-    const changeLanguage = (lang: Language) => {
-        setLanguage(lang);
-    };
-
-    const text = translations[language];
-
-    return (
-        <nav className="navbar">
-            {/* Logo */}
-           <div className="navbar-logo">
-                <h3>Logo</h3>
-            </div>
-
-            {/* Nav Links */}
-            <div className="navbar-links">
-                <a href="#about">{text.about}</a>
-                <a href="#skills">{text.skills}</a>
-                <a href="#projects">{text.projects}</a>
-                <a href="#contact">{text.contact}</a>
-            </div>
-
-            {/* Actions */}
-            <div className= "darkMode-toggle">
-                <button onClick={toggleDarkMode}>
-                    Dark Mode 
-                </button>
-            </div> 
-
-            <div className="language-toggle">
-                <button onClick={() => changeLanguage('en')}>
-                    EN
-                </button>
-                <button onClick={() => changeLanguage('es')}>
-                    ES
-                </button>
-            </div>
-        </nav>
-    );
-
-}
-export default Navbar
+    export default Navbar
